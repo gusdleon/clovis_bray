497e3872a331e095dc7b7a6adf651e2d38d8dadc
Gustavo de León <alfonso.gus.deleon@gmail.com>
2022-09-16 03:04:45 -0500

    Cambio de la configuración del cache
    Aumentada la duración del cache de 100 segundos a un día

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ export default {
 			// Cache API respects Cache-Control headers. Setting s-max-age to 10
 			// will limit the response to be in cache for 10 seconds max
 			// Any changes made to the response here will be reflected in the cached value
-			headers.append('Cache-Control', 's-maxage=100');
+			headers.append('Cache-Control', 's-maxage=86400');
 
 			response = new Response(object.body, {
 				headers,
